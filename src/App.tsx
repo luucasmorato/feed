b24f5b7d3a318479958e3274c12ae7a99e035e2c
Lucas Morato <luucasmorato@gmail.com>
2022-10-08 14:53:40 -0300

    create project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Header } from "./components/header";
+import { Home } from "./pages/Home";
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  );
+}
