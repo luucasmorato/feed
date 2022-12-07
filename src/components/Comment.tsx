d98ebdd6e8e69c34eb58099107d2f837c21ae876
Lucas Morato <luucasmorato@gmail.com>
2022-12-06 21:59:56 -0300

    create comment compoent layout

---
+++ src/components/Comment.tsx
@@ -0,0 +1,36 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/luucasmorato.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Lucas Morato</strong>
+              <time
+                title="11 de Março às 13:15h"
+                dateTime="2022-03-11 13:15:00"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp /> Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
